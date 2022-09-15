22fe5879cab0a75ce13b4e2ea3f5ae33a9bfb537
WalobwaD <walobwadan@gmail.com>
2022-09-15 12:00:53 -0700

    Initial commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
-* main - Prints _putchar
-*
-* Return: Always 0 (Success)
-*/
+ * main - Prints Holberton
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-        _putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
-        return(0);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
 }
